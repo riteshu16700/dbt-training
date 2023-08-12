914ce847ed89980dfce4bb86dc2a09da67e46533
riteshu16700@gmail.com <riteshu16700@gmail.com>
2023-08-12 23:26:40 +0000

    developing stage orders

---
+++ models/staging/stage_orders.sql
@@ -0,0 +1,22 @@
+select 
+--from raw_orders
+O.orderid,
+O.orderdate,
+O.shipdate,
+O.shipmode, 
+O.ORDERSELLINGPRICE - O.ODERCOSTPRICE as orderprofit,
+O.ODERCOSTPRICE,
+O.ORDERSELLINGPRICE,
+-- from raw_customer
+c.customername,
+c.segment,
+c.country,
+-- from raw_product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('RAW_ORDERS') }}  as O
+left join {{ ref('RAW_CUSTOMER') }} as c
+on O.CUSTOMERID = c.CUSTOMERID
+left join {{ ref('RAW_PRODUCT') }} as P
+on O.PRODUCTID = p.PRODUCTID
